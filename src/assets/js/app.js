efb0c780390965697f85da8e2a99c3bfade91287
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-10-10 21:22:35 +1000

    Header updated

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,3 +6,49 @@ import './utils/polyfill';
 //modules
 import './modules/PageHeader';
 import './modules/Slider';
+import { createPopper } from '@popperjs/core';
+
+import {TweenLite} from "gsap";
+
+document.addEventListener('DOMContentLoaded', function () {
+});
+
+let dropDown;
+
+// document.addEventListener('mousemove', function(e) {
+//   const navtoggle = e.target.closest('.main-nav__link');
+//
+//   if (navtoggle) {
+//
+//     dropDown = navtoggle.nextElementSibling;
+//
+//     if (dropDown) {
+//
+//       TweenLite.to(dropDown, 0.25, {
+//         display: 'block',
+//         opacity: 1
+//       });
+//
+//       // createPopper(navtoggle, dropDown, {
+//       //   placement: 'top-start',
+//       //   modifiers: [
+//       //     {
+//       //       name: 'offset',
+//       //       options: {
+//       //         offset: ({ placement, reference, popper }) => {
+//       //           return [0, 40];
+//       //         }
+//       //       },
+//       //     },
+//       //   ],
+//       // });
+//
+//     }
+//
+//   } else {
+//     TweenLite.to(dropDown, 0, {
+//       display: 'none',
+//       opacity: 0
+//     });
+//   }
+// });
