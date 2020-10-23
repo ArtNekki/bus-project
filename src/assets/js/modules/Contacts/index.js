1f45f0f067684d1b5267f4607551a1bb335020ca
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-10-23 17:57:22 +1000

    Map fixed for contacts page

--- src/assets/js/modules/Contacts/index.js
+++ src/assets/js/modules/Contacts/index.js
@@ -2,6 +2,15 @@ import contactCoords from '../../../../data/contactCoords.json';
 import initMap from "../Map";
 import Tabby from "tabbyjs";
 
+function initTabs(breakpoint) {
+  let tabs = new Tabby('[data-contact-tabs] ul')
+
+  if (breakpoint.matches) {
+    tabs.destroy()
+  } else {
+    tabs = new Tabby('[data-contact-tabs] ul');
+  }
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   const contactsMap = document.getElementById('contactsMap');
@@ -15,8 +24,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const tabsContainer = document.querySelector('[data-contact-tabs]');
 
   if (tabsContainer) {
-    const tabs = new Tabby('[data-contact-tabs] ul');
-    // tabs.toggle('#boxBerry');
+    const breakpoint = window.matchMedia(`(min-width: 768px)`);
+    breakpoint.addListener(initTabs);
+
+    initTabs(breakpoint);
   }
 
 });
