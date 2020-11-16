c3451218e68ebae2f6c5a6b86694b4fc64811145
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-11-16 19:04:41 +1000

    Radio button refatored

---
+++ src/helpers/radio.js
@@ -0,0 +1,43 @@
+module.exports = function(options) {
+  const id = options.hash.id;
+  const name = options.hash.name;
+  const help = options.hash.help;
+  const meta = options.hash.meta;
+  const mods = options.hash.mods;
+  const root = options.data.root.root;
+  let cssClass = 'radio';
+  let allMods = '';
+
+  if (mods !== 'undefined' && mods ) {
+    const modsList = mods.split(',');
+      for (let i = 0; i < modsList.length; i++) {
+        allMods = allMods + ' radio--' + modsList[i].trim();
+      }
+  }
+
+  cssClass+= allMods;
+
+  function renderHelp() {
+    return `<button type="button" class="radio__help">
+        <svg class="icon" width="18" height="18">
+          <use xlink:href="${root}assets/img/symbol/sprite.svg#help"></use>
+        </svg>
+    </button>`;
+  }
+
+  function renderMeta(data) {
+    return `<span class="radio__meta">${data}</span>`
+  }
+
+   const radio  = `
+      <label class="${cssClass}">
+        <input type="radio" id="${id}" name="${name}" class="radio__input">
+        <div class="radio__body">
+            <span class="radio__label">Автозапчасти</span>
+            ${ meta ? renderMeta(meta) : ''}
+            ${ help ? renderHelp() : ''}
+        </div>
+      </label>`
+
+  return radio;
+}
