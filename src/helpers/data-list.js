8c915ebb0517545f42c3e81b314df046f8f70a58
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-12-22 11:15:28 +1000

    Order-list renamed to data-list

--- src/helpers/data-list.js
+++ src/helpers/data-list.js
@@ -2,21 +2,21 @@ module.exports = function(options) {
   const id = options.hash.id;
   const items = JSON.parse(options.hash.items);
   const mods = options.hash.mods;
-  let cssClass = 'order-list';
+  let cssClass = 'data-list';
   let allMods = '';
 
   if (mods !== 'undefined' && mods ) {
     const modsList = mods.split(',');
       for (let i = 0; i < modsList.length; i++) {
-        allMods = allMods + ' order-list--' + modsList[i].trim();
+        allMods = allMods + ' data-list--' + modsList[i].trim();
       }
   }
 
   function renderItems(data) {
     return data.map((item) => {
-      return `<li class="order-list__item">
-        <span class="order-list__name">${item.name}</span>
-        <span class="order-list__value">${item.value}</span>
+      return `<li class="data-list__item">
+        <span class="data-list__name">${item.name}</span>
+        <span class="data-list__value">${item.value}</span>
       </li>`
     }).join(``);
   }
