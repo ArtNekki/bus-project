06f35b675799c50a1488e7c53d29240b4e476bce
Anton Mayatskiy <work.mayatskiy@gmail.com>
2021-02-02 19:43:30 +1000

    KCE map done

--- src/helpers/img.js
+++ src/helpers/img.js
@@ -20,11 +20,10 @@ module.exports = function(options) {
   cssClass+= allMods;
 
   return `<picture>
-    ${breakpoints.xxl ? `<source media="(min-width: 1440px)" srcset="${root}assets/img/content/${name}--xxl.${ext} 1x, ${root}assets/img/content/${name}--xxl@2x.${ext} 2x, ${root}assets/img/content/${name}--xxl@3x.${ext} 3x">` : ``}
-    ${breakpoints.xl ? `<source media="(min-width: 1280px)" srcset="${root}assets/img/content/${name}--xl.${ext} 1x, ${root}assets/img/content/${name}--xl@2x.${ext} 2x, ${root}assets/img/content/${name}--xl@3x.${ext} 3x">` : ``}
-    ${breakpoints.lg ? `<source media="(min-width: 900px)" srcset="${root}assets/img/content/${name}--lg.${ext} 1x, ${root}assets/img/content/${name}--lg@2x.${ext} 2x, ${root}assets/img/content/${name}--lg@3x.${ext} 3x">` : ``}
+    ${breakpoints.xl ? `<source media="(min-width: 1200px)" srcset="${root}assets/img/content/${name}--xl.${ext} 1x, ${root}assets/img/content/${name}--xl@2x.${ext} 2x, ${root}assets/img/content/${name}--xl@3x.${ext} 3x">` : ``}
+    ${breakpoints.lg ? `<source media="(min-width: 992px)" srcset="${root}assets/img/content/${name}--lg.${ext} 1x, ${root}assets/img/content/${name}--lg@2x.${ext} 2x, ${root}assets/img/content/${name}--lg@3x.${ext} 3x">` : ``}
     ${breakpoints.md ? `<source media="(min-width: 768px)" srcset="${root}assets/img/content/${name}--md.${ext} 1x, ${root}assets/img/content/${name}--md@2x.${ext} 2x, ${root}assets/img/content/${name}--md@3x.${ext} 3x">` : ``}
-    ${breakpoints.sm ? `<source media="(min-width: 600px)" srcset="${root}assets/img/content/${name}--sm.${ext} 1x, ${root}assets/img/content/${name}--sm@2x.${ext} 2x, ${root}assets/img/content/${name}--sm@3x.${ext} 3x">` : ``}
+    ${breakpoints.sm ? `<source media="(min-width: 576px)" srcset="${root}assets/img/content/${name}--sm.${ext} 1x, ${root}assets/img/content/${name}--sm@2x.${ext} 2x, ${root}assets/img/content/${name}--sm@3x.${ext} 3x">` : ``}
     <img src="${root}assets/img/content/${name}--xs.${ext}" srcset="${root}assets/img/content/${name}--xs@2x.${ext} 2x, ${root}assets/img/content/${name}--xs@3x.${ext} 3x" ${width ? `width="${width}"` : ``} ${height ? `height="${height}"` : ``} alt="${alt}">
   </picture>`
 }
